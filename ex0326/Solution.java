91b92ec072c389095a5d0074cacc45d368a483f8
Park <jeongui15@naver.com>
2020-03-26 19:18:19 +0900

    유니폼 빌려주기

---
+++ ex0326/Solution.java
@@ -0,0 +1,35 @@
+package eeeeee;
+
+
+class Solution {
+   public static void main(String[] args) {
+      
+   
+    //public int solution(int n, int [] lost, int [] reserve) {
+        int answer = 0;
+       // int sum=0;
+        int[] lost = {2,4};
+        int[] reserve = {1,3,5};
+        for(int i=0; i< lost.length; i++) {
+           //sum = sum +i;
+        for(int j=0; j<reserve.length; j++);
+              //System.out.println("이거맞음?");
+           //if (int [] lost === int [] reserve);
+              //sum = sum +j;
+           //}else if n
+        for (int [] lost = i; );
+        for (int [] reserve= j;);
+              /*
+               * 3번 학생이 2번 학생이나 4번 학생에게 체육복을 빌려주면
+               *  학생 4명이 체육수업을 들을 수 있습니다
+               */
+                 
+              /*1번 학생이 2번 학생에게 체육복을 빌려주고, 3번 학생이나 5번 학생이 4번 학생에게 
+               * 체육복을 빌려주면 학생 5명이 체육수업을 들을 수 있습니다.*/
+                 for()
+              return answer;
+
+    }
+}
+        
+} 
